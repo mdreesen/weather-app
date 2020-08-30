34772faf44e3a48f10148dd1674c8fd5a6af67b1
Michael Dreesen <michaeldreesen90@gmail.com>
2020-08-30 15:09:32 -0600

    got degree

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,41 @@
-// VARIABLES
-//var
-
 // FETCHING THE URL
-var wUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=b6a498bf6c1a44cb20137595567af9c8"
+function today() {
 
-var today = function() {
+    var city = document.querySelector('#city').value;
     // Fetching the URL
-    fetch(wUrl)
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=b6a498bf6c1a44cb20137595567af9c8&units=imperial')
         .then(function(response) {
+            console.log(response);
+
             return response.json();
         })
-        .then(function(data) {
-            console.log(data.list[0])
-        })
-        /*
-        // this is where the weather information will go
         .then(function(response) {
-            var todayContainerEl = document.querySelector("#now")
-            var todayElement = document.createElement("div")
-            todayElement.setAttribute('p', response.data.url);
-            todayContainerEl.appendChild(todayElement);
+            console.log(response);
+
+            // create a variable the will select the div where the temp will be placed
+            var tempContainer = document.querySelector("#now");
+            // Empty out the div
+            tempContainer.innerHTML = "";
+            var degree = document.createElement('div');
+            degree.textContent = response.main.temp;
+            // degree.setAttribute('src', data.main.temp);
+            tempContainer.appendChild(degree);
+
         })
-        */
 }
 
-today();
+function fiveDay() {
+    var city = document.querySelector('#city').value;
+
+    fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&APPID=b6a498bf6c1a44cb20137595567af9c8&units=imperial')
+        .then(function(response) {
+            console.log(response);
+            return response.json;
+        })
+        .then(function(data) {
+            console.log(data);
+        });
+}
+
+
+// var weather = document.addEventListener("click", show)
